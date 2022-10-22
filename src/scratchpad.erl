fd40d7942ee8ca1a5df34d7902ce87a516b0a693
Hussain Imthiaz Hussain <imthiazdps@gmail.com>
2022-10-22 19:08:24 -0400

    Start

---
+++ src/scratchpad.erl
@@ -0,0 +1,21 @@
+%%%-------------------------------------------------------------------
+%%% @author User
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 20. Oct 2022 3:06 PM
+%%%-------------------------------------------------------------------
+-module(scratchpad).
+-author("User").
+
+%% API
+-export([main/0]).
+
+main() ->
+%%  crypto:sha(integer_to_binary(123)).
+  Abc = 1,
+  Def = 2,
+  NodesList = [3,4],
+  [7|NodesList].
+
